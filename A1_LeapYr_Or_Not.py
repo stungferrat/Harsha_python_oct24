87cf25777493fe75644da644aae7265c7cb59c8b
stungferrat <harshams15@gmail.com>
2025-09-25 17:21:35 +0530

    Update A1_LeapYr_Or_Not.py

--- A1_LeapYr_Or_Not.py
+++ A1_LeapYr_Or_Not.py
@@ -1,5 +1,2 @@
-year= int(input("Enter year: "))
-if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-  print(f"{year} is a leap year.")
-else:
-  print(f"{year} is not a leap year.")
+print("Hello")
+print("checking commits")
